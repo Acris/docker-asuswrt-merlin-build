3e8dc77ed2f10bcdfe142cd932a0dd6b2539ab08
Acris Liu <acrisliu@gmail.com>
2019-03-24 02:15:18 +0800

    Update bcm-hnd.sh

--- env/bcm-hnd.sh
+++ env/bcm-hnd.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cd /root
-git clone https://github.com/RMerl/am-toolchains.git
-mkdir /opt
 ln -s /root/am-toolchains/brcm-arm-hnd /opt/toolchains
 export LD_LIBRARY_PATH=$LD_LIBRARY:/opt/toolchains/crosstools-arm-gcc-5.3-linux-4.1-glibc-2.22-binutils-2.25/usr/lib
 export TOOLCHAIN_BASE=/opt/toolchains
