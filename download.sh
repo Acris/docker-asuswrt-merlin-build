0d0da447e25e97c0898180c991471b8581a06ae4
Acris Liu <acrisliu@gmail.com>
2019-03-23 23:13:56 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ASUSWRT_MERLIN_VERSION=384.9
+
+cd /tmp
+wget https://github.com/RMerl/asuswrt-merlin.ng/archive/${ASUSWRT_MERLIN_VERSION}.tar.gz
+tar -zxf ${ASUSWRT_MERLIN_VERSION}.tar.gz
+rm ${ASUSWRT_MERLIN_VERSION}.tar.gz
+mv asuswrt-merlin.ng-${ASUSWRT_MERLIN_VERSION} /home/asuswrt-merlin
+bash /root/build.sh
