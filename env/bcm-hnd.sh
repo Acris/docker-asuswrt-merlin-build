e700c8edfefb1a30a26f089a826ca35df0ad8296
Acris Liu <acrisliu@gmail.com>
2019-03-24 00:38:33 +0800

    Update and rename env.sh to env/bcm-hnd.sh

--- env/bcm-hnd.sh
+++ env/bcm-hnd.sh
@@ -7,6 +7,3 @@ export LD_LIBRARY_PATH=$LD_LIBRARY:/opt/toolchains/crosstools-arm-gcc-5.3-linux-
 export TOOLCHAIN_BASE=/opt/toolchains
 export PATH=$PATH:/opt/toolchains/crosstools-arm-gcc-5.3-linux-4.1-glibc-2.22-binutils-2.25/usr/bin
 export PATH=$PATH:/opt/toolchains/crosstools-aarch64-gcc-5.3-linux-4.1-glibc-2.22-binutils-2.25/usr/bin
-ln -s /root/am-toolchains/brcm-arm-sdk/hndtools-arm-linux-2.6.36-uclibc-4.5.3 /opt/brcm-arm
-ln -s /root/am-toolchains/brcm-arm-sdk /home/asuswrt-merlin/release/src-rt-6.x.4708/toolchains
-export PATH=$PATH:/opt/brcm-arm/bin
