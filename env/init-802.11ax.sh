d1ad42fc4a89b857c623c1128049edde7817d493
Acris Liu <acrisliu@gmail.com>
2019-03-24 05:47:55 +0800

    Update and rename bcm-hnd-802.11ax.sh to init-802.11ax.sh

--- env/init-802.11ax.sh
+++ env/init-802.11ax.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-ln -s /root/am-toolchains/brcm-arm-hnd /opt/toolchains
+ln -sf /root/am-toolchains/brcm-arm-hnd /opt/toolchains
 export LD_LIBRARY_PATH=$LD_LIBRARY:/opt/toolchains/crosstools-arm-gcc-5.5-linux-4.1-glibc-2.26-binutils-2.28.1/usr/lib
 export TOOLCHAIN_BASE=/opt/toolchains
 export PATH=$PATH:/opt/toolchains/crosstools-arm-gcc-5.5-linux-4.1-glibc-2.26-binutils-2.28.1/usr/bin
 export PATH=$PATH:/opt/toolchains/crosstools-aarch64-gcc-5.5-linux-4.1-glibc-2.26-binutils-2.28.1/usr/bin
+
+ln -sf /root/am-toolchains/brcm-arm-sdk/hndtools-arm-linux-2.6.36-uclibc-4.5.3 /opt/brcm-arm
+ln -sf /root/am-toolchains/brcm-arm-sdk /home/asuswrt-merlin/release/src-rt-6.x.4708/toolchains
+export PATH=$PATH:/opt/brcm-arm/bin
