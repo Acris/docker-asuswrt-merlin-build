c2b89f333dd8eb71e80957092430b75e07c83354
Acris Liu <acrisliu@gmail.com>
2019-03-24 05:48:02 +0800

    Delete bcm-sdk.sh

--- env/bcm-sdk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ln -s /root/am-toolchains/brcm-arm-sdk/hndtools-arm-linux-2.6.36-uclibc-4.5.3 /opt/brcm-arm
-ln -sf /root/am-toolchains/brcm-arm-sdk /home/asuswrt-merlin/release/src-rt-6.x.4708/toolchains
-export PATH=$PATH:/opt/brcm-arm/bin
