7c29f420c0c900aceb6fcd1a7e0bac2c5833918d
Acris Liu <acrisliu@gmail.com>
2019-03-24 00:38:30 +0800

    Create bcm-hnd-802.11ax.sh

---
+++ env/bcm-hnd-802.11ax.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /root
+git clone https://github.com/RMerl/am-toolchains.git
+mkdir /opt
+ln -s /root/am-toolchains/brcm-arm-hnd /opt/toolchains
+export LD_LIBRARY_PATH=$LD_LIBRARY:/opt/toolchains/crosstools-arm-gcc-5.5-linux-4.1-glibc-2.26-binutils-2.28.1/usr/lib
+export TOOLCHAIN_BASE=/opt/toolchains
+export PATH=$PATH:/opt/toolchains/crosstools-arm-gcc-5.5-linux-4.1-glibc-2.26-binutils-2.28.1/usr/bin
+export PATH=$PATH:/opt/toolchains/crosstools-aarch64-gcc-5.5-linux-4.1-glibc-2.26-binutils-2.28.1/usr/bin
