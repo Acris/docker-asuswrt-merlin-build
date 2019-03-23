cff719fb1405bc297b0e08d5a4d4a53e22617611
Acris Liu <acrisliu@gmail.com>
2019-03-24 02:15:11 +0800

    Update bcm-sdk.sh

--- env/bcm-sdk.sh
+++ env/bcm-sdk.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cd /root
-git clone https://github.com/RMerl/am-toolchains.git
-mkdir /opt
 ln -s /root/am-toolchains/brcm-arm-sdk/hndtools-arm-linux-2.6.36-uclibc-4.5.3 /opt/brcm-arm
 ln -s /root/am-toolchains/brcm-arm-sdk /home/asuswrt-merlin/release/src-rt-6.x.4708/toolchains
 export PATH=$PATH:/opt/brcm-arm/bin
