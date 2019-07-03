7ef0a7949115d25dfb6099e6aa897a576bc8bb0e
Acris Liu <acrisliu@gmail.com>
2019-07-04 01:47:14 +0800

    Remove download.sh

--- env/bcm-sdk.sh
+++ env/bcm-sdk.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ln -sf /root/am-toolchains/brcm-arm-sdk/hndtools-arm-linux-2.6.36-uclibc-4.5.3 /opt/brcm-arm
-ln -sf /root/am-toolchains/brcm-arm-sdk /home/asuswrt-merlin/release/src-rt-6.x.4708/toolchains
 
 export LD_LIBRARY_PATH=$LD_LIBRARY:/opt/brcm-arm/lib
 export PATH=$PATH:/opt/brcm-arm/bin
